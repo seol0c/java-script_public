9fb30bb7927e5d8336888726441177268fef17f3
seol0c <youngchanseol@gmail.com>
2025-11-30 23:23:54 +0000

    update

--- food/2. refresh.js
+++ food/2. refresh.js
@@ -1,11 +1,17 @@
-function autoRefresh() {
+function autoRefresh2() {
   var sh = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("DB");
+
+  // 기존 값 삭제
   sh.getRange("C3:C9").clearContent();
-  Utilities.sleep(5000);
-  //sh.getRange("C3").setValue("=TODAY_BREAKFAST()"); // 오늘 아침
-  sh.getRange("C4").setValue("=TODAY_LUNCH()");
-  sh.getRange("C5").setValue("=TODAY_DINNER()");
-  //sh.getRange("C7").setValue("=TOMORROW_BREAKFAST()"); // 내일 아침
-  sh.getRange("C8").setValue("=TOMORROW_LUNCH()");
-  sh.getRange("C9").setValue("=TOMORROW_DINNER()");
+
+  // 스크립트 내부에서 직접 함수 실행하고 결과값만 셀에 넣기
+  // 오늘
+  // sh.getRange("C3").setValue(TODAY_BREAKFAST());
+  sh.getRange("C4").setValue(TODAY_LUNCH());
+  sh.getRange("C5").setValue(TODAY_DINNER());
+
+  // 내일
+  // sh.getRange("C7").setValue(TOMORROW_BREAKFAST());
+  sh.getRange("C8").setValue(TOMORROW_LUNCH());
+  sh.getRange("C9").setValue(TOMORROW_DINNER());
 }
